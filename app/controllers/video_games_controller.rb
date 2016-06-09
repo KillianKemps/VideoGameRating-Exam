b106a43030ffc3eedb8a23de6f6b457e6374a336
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-06-09 12:14:55 +0200

    Add minimal display on index

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -1,9 +1,17 @@
 class VideoGamesController < ApplicationController
   def add
+    # No idea why 'create' method doesn't work
+    @video_game = VideoGame.create(name: 'test name', editor: 'test editor', description: 'test descr')
   end
 
   def index
     @video_games = VideoGame.all
+    if not @video_games.any?
+      @video_games = [
+        VideoGame.new(name: 'Some beautiful name', editor: 'Editor name should be here', description: 'Awesome description'),
+        VideoGame.new(name: 'Other test name', editor: 'Greatest editor', description: 'Test description'),
+      ]
+    end
   end
 
   def show
