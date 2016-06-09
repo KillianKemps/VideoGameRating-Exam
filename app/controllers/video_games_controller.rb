f4f5f064f5be88677c6377667d544fa163e775a0
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-06-09 11:29:14 +0200

    Add video games controller and routes

---
+++ app/controllers/video_games_controller.rb
@@ -0,0 +1,10 @@
+class VideoGamesController < ApplicationController
+  def add
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+
+  root 'video_games#index'
+
+  get 'video_games/add'
+
+  get 'video_games/index'
+
+  get 'video_games/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
