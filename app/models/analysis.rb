cf8338374b76706646f8481603d32a69cb186907
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-06-09 10:56:20 +0200

    Add relation between videogame and analysis

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,6 +1,8 @@
 class Analysis < ActiveRecord::Base
   attr_reader :content
 
+  belongs_to :video_game
+
   def initialize(attributes)
     @content  = attributes[:content]
   end
--- app/models/video_game.rb
+++ app/models/video_game.rb
@@ -1,6 +1,8 @@
 class VideoGame < ActiveRecord::Base
   attr_reader :name, :editor, :description
 
+  has_many :analysis
+
   def initialize(attributes)
     @name  = attributes[:name]
     @editor  = attributes[:editor]
